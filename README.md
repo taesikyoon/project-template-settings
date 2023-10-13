6113eb8f3e726f2153ea27e50770d94dc931613f
taesik <taesikyoon97@naver.com>
2023-10-13 16:13:02 +0900

    update READAME - environment template

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 
 # 목차
 ### - [로그 템플릿](#로그-템플릿)
+### - [환경변수 템플릿](#환경변수-템플릿)
 
    
 ## 로그 템플릿
 로그를 남기기 위한 윈스턴 라이브러리 활용 또는 프레임워크 활용이 있습니다.
-- [Logger with Winston README.md 가기 ](https://github.com/taesikyoon/project-template-settings/blob/main/logger-winston/README.md)
+- [Logger with Winston README.md 가기 ](https://github.com/taesikyoon/project-template-settings/blob/main/logger-winston/README.md)   
+   
+## 환경변수 템플릿
+dev, dev, prod 각 환경에 맞는 실행을 위한 작업과 class-validator 라이브러리를 활용한 환경변수에 대한 유효성 검사
+- [Environment README.md 가기 ](https://github.com/taesikyoon/project-template-settings/blob/main/environment/README.md)
--- environment/src/main.ts
+++ environment/src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AppConfig } from './environment/values/app';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const appConfig: AppConfig = configService.get('APP');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(appConfig.APP_PORT);
 }
