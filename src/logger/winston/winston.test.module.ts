92656356c4f2540b231bf285f62a848f04513c4a
taesik <taesikyoon97@naver.com>
2023-10-13 14:59:41 +0900

    Delete Logger on main path

--- src/logger/winston/winston.test.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LoggingInterceptor } from './interceptor/winston-logger.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-
-@Module({
-  controllers: [],
-  providers: [{ provide: APP_INTERCEPTOR, useClass: LoggingInterceptor }],
-})
-export class WinstonLoggingModule {}
